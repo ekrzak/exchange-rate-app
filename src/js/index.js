6682fb443a6cd192c0a278d59c44110e6b375a11
estebankrzak <esteban.rk@hotmail.com>
2022-03-24 13:24:19 -0300

    Refactoring code

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,25 @@
-fetch('https://v6.exchangerate-api.com/v6/b2aa2900d00de3bb4c3db731/latest/USD')
-  .then(response => response.json())
-  .then(data => {
-    document.querySelector('#spinner').classList.add('hidden');
-    document.querySelector('#description').classList.remove('hidden');
-    document.querySelector('#description-base').textContent = `${data['base_code']}`;
-    document.querySelector('#description-rate-date').textContent = `${data['time_last_update_utc'].slice(0, 16)}`;
-    Object.entries(data['conversion_rates']).forEach(function(pairs) {
-      const newTr = document.createElement('tr');
-      const currencyTd = document.createElement('td');
-      const rateTd = document.createElement('td');
-      currencyTd.textContent = `${pairs[0]}`;
-      rateTd.textContent = `${pairs[1]}`;
-      newTr.appendChild(currencyTd);
-      newTr.appendChild(rateTd);
-      document.querySelector('#data-table').appendChild(newTr);
+function searchRatesInApi() {
+  return fetch('https://v6.exchangerate-api.com/v6/b2aa2900d00de3bb4c3db731/latest/USD')
+    .then(response => response.json())
+    .catch(e => console.error(e))
+}
 
-    });
+function printDataInDom(dataObject) {
+  document.querySelector('#spinner').classList.add('hidden');
+  document.querySelector('#description').classList.remove('hidden');
+  document.querySelector('#description-base').textContent = `${dataObject['base_code']}`;
+  document.querySelector('#description-rate-date').textContent = `${dataObject['time_last_update_utc'].slice(0, 16)}`;
+  Object.entries(dataObject['conversion_rates']).forEach(function(pairs) {
+    const newTr = document.createElement('tr');
+    const currencyTd = document.createElement('td');
+    const rateTd = document.createElement('td');
+    currencyTd.textContent = `${pairs[0]}`;
+    rateTd.textContent = `${pairs[1]}`;
+    newTr.appendChild(currencyTd);
+    newTr.appendChild(rateTd);
+    document.querySelector('#data-table').appendChild(newTr);
+
   });
+}
+
+searchRatesInApi().then(data => printDataInDom(data));
