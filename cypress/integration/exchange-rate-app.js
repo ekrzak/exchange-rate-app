777fb33070793dfd0933b436ecb33761b2e84dd8
estebankrzak <esteban.rk@hotmail.com>
2022-03-29 17:22:57 -0300

    Add some cypress tests

--- cypress/integration/exchange-rate-app.js
+++ cypress/integration/exchange-rate-app.js
@@ -1,2 +1,26 @@
 /// <reference types="Cypress" />
 
+const NUMBER_OF_CURRENCIES = 161;
+const d = new Date();
+
+context('Exchange Rate App', () => {
+  
+  before(() => {
+    cy.visit('/');
+  });
+
+  describe('Home page', () => {
+    
+    it('shows the base currency in the table description', () => {
+      cy.get('#description-base').should('have.text', 'USD');
+    });
+
+    it('shows the current date in the table description', () => {
+      cy.get('#description-rate-date').contains(d.getDate());
+    });
+
+    it('shows a list of currency rates for USD base', () => {
+      cy.get('#data-table').find('tr').should('have.length', NUMBER_OF_CURRENCIES);
+    });
+  });
+});
