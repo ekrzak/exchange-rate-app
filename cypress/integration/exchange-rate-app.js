af873c5880ed713ce31d28d724a55b381cf847eb
estebankrzak <esteban.rk@hotmail.com>
2022-03-29 18:17:35 -0300

    Add cypress tests for dropdown menu

--- cypress/integration/exchange-rate-app.js
+++ cypress/integration/exchange-rate-app.js
@@ -19,8 +19,38 @@ context('Exchange Rate App', () => {
       cy.get('#description-rate-date').contains(d.getDate());
     });
 
+    it('shows the rate for the base currency', () => {
+      cy.get('#data-table tr').first().should('have.text', 'USD1');
+    });
+
     it('shows a list of currency rates for USD base', () => {
       cy.get('#data-table').find('tr').should('have.length', NUMBER_OF_CURRENCIES);
     });
   });
+
+  describe('View for another currency', () => {
+    
+    it('picks a currency in the dropdown menu', () => {
+      cy.get('#navbarDarkDropdownMenuLink').click();
+      cy.contains('ARS').click();
+    });
+
+    it('shows the base currency in the table description', () => {
+      cy.get('#description-base').should('have.text', 'ARS');
+    });
+
+    it('shows the current date in the table description', () => {
+      cy.get('#description-rate-date').contains(d.getDate());
+    });
+
+    it('shows the rate for the base currency', () => {
+      cy.get('#data-table tr').first().should('have.text', 'ARS1');
+    });
+
+    it('shows a list of currency rates for ARS base', () => {
+      cy.get('#data-table').find('tr').should('have.length', NUMBER_OF_CURRENCIES);
+    });
+
+  });
+
 });
